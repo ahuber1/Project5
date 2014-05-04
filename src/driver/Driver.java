7a4e1f8bc335f5d1c7caa01f75dc37641e5c2fe3
ahuber1 <andrewhuber93@icloud.com>
2014-05-03 23:54:14 -0400

    Update Driver.java
    Now main tests all possible combinations of i and j

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -5,15 +5,19 @@ import hubble.Handler;
 public class Driver {
 	
 	public static void main(String[] args) {
-		int[] i = {8, 8, 8, 11, 11};
+		int[] i = {8, 9, 10, 11};
 		int[] j = {1, 2, 3, 4, 5};
-		Handler[] handlers = new Handler[i.length];
-		Thread[] threads = new Thread[i.length];
+		Handler[] handlers = new Handler[i.length * j.length];
+		Thread[] threads = new Thread[i.length * j.length];
+		int indexVal = 0;
 		
-		for(int index = 0; index < handlers.length; index++) {
-			handlers[index] = new Handler(index + 1, i[index], j[index]);
-			threads[index] = new Thread(handlers[index]);
-			threads[index].start();
+		for(int index = 0; index < i.length; index++) {
+			for(int index2 = 0; index2 < j.lengthl index2++) {
+				handlers[indexVal] = new Handler(indexVal + 1, i[index], j[index2]);
+				threads[indexVal] = new Thread(handlers[indexVal]);
+				threads[indexVal].start();
+				indexVal++;
+			}
 		}
 		
 		for(int index = 0; index < threads.length; index++) {
